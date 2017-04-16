4cfb18b9577a30052e5f1729cb1852825c54d76f
M. Willis Monroe <willismonroe@gmail.com>
2017-04-16 10:55:53 -0700

    set up classes for terminal, screen, and editor

--- kilo.py
+++ kilo.py
@@ -3,6 +3,8 @@ import sys
 import os
 import termios
 
+import time
+
 # helpful module: https://github.com/stefanholek/term/blob/master/term/__init__.py
 
 # Helper prefixes
@@ -19,91 +21,124 @@ ISPEED = 4
 OSPEED = 5
 CC = 6
 
-def setraw(fd, when=termios.TCSAFLUSH, min=1, time=0):
-    """Puts current terminal into raw mode."""
-
-    mode = termios.tcgetattr(fd)
-
-    mode[IFLAG] = mode[IFLAG] & ~(termios.BRKINT | termios.ICRNL | termios.INPCK | termios.ISTRIP | termios.IXON)
-    mode[OFLAG] = mode[OFLAG] & ~(termios.OPOST)
-    mode[CFLAG] = mode[CFLAG] | termios.CS8
-    mode[LFLAG] = mode[LFLAG] & ~(termios.ECHO | termios.ICANON | termios.IEXTEN | termios.ISIG)
-    mode[CC][termios.VMIN] = 0
-    mode[CC][termios.VTIME] = 1
-
-    termios.tcsetattr(fd, when, mode)
-
-class rawmode(object):
-    """Context manager for raw mode"""
-
-    def __init__(self, fd, when=termios.TCSAFLUSH, min=1, time=0):
-        self.fd = fd
-        self.when = when
-        self.min = min
-        self.time = time
-
-    def __enter__(self):
-        self.savedmode = termios.tcgetattr(self.fd)
-        setraw(self.fd, self.when, self.min, self.time)
-
-    def __exit__(self, *ignored):
-        termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.savedmode)
-
-def _opentty(device, bufsize):
-    """Open a tty for reading and writing"""
-    fd = os.open(device, os.O_RDWR | os.O_NOCTTY)
-
-    if sys.version_info[0] >= 3:
-
-        try:
-            os.lseek(fd, 0, os.SEEK_CUR)
-        except OSError:
-            bufsize = 0
-        return open(fd, 'rb+', bufsize)
-
-    return os.fdopen(fd, 'r+', bufsize)
-
-class opentty(object):
-    """Context manager for opening a tty safely"""
-
-    device = '/dev/tty'
+class Terminal():
+    """Class dealing with all aspects of writing and reading a tty connection."""
 
     def __init__(self, bufsize=1):
         self.bufsize = bufsize
+        self.tty = self.open_tty(bufsize=self.bufsize)
 
-    def __enter__(self):
-        self.tty = None
+    def open_tty(self, device='/dev/tty', bufsize=1):
+        fd = os.open(device, os.O_RDWR | os.O_NOCTTY)
 
-        try:
-            self.tty = _opentty(self.device, self.bufsize)
-        except EnvironmentError:
-            pass
-        return self.tty
+        if sys.version_info[0] >= 3:
 
-    def __exit__(self, *ignored):
-        if self.tty is not None:
-            self.tty.close()
+            try:
+                os.lseek(fd, 0, os.SEEK_CUR)
+            except OSError:
+                bufsize = 0
+            return open(fd, 'rb+', bufsize)
 
-def _readyx(stream):
-    """Read a CSI """
-    output = b''
-    # read one byte from the stream
-    c = stream.read(1)
+        return os.fdopen(fd, 'r+', bufsize)
 
-    while c:
-        output += c
-        if c == b'R':
-            break
-        c = stream.read(1)
+    def close_tty(self):
+        self.tty.close()
 
-    if output:
-        print('output: {}'.format(output))
-        line, col = map(int, output[2:-1].decode('ascii').split(';'))
-        return (line, col)
+    def set_raw(self, when=termios.TCSAFLUSH, min=1, time=0):
+        """Puts current terminal into raw mode."""
+        self.savedmode = termios.tcgetattr(self.tty)
+
+        mode = termios.tcgetattr(self.tty)
+
+        mode[IFLAG] = mode[IFLAG] & ~(termios.BRKINT | termios.ICRNL | termios.INPCK | termios.ISTRIP | termios.IXON)
+        mode[OFLAG] = mode[OFLAG] & ~(termios.OPOST)
+        mode[CFLAG] = mode[CFLAG] | termios.CS8
+        mode[LFLAG] = mode[LFLAG] & ~(termios.ECHO | termios.ICANON | termios.IEXTEN | termios.ISIG)
+        mode[CC][termios.VMIN] = 0
+        mode[CC][termios.VTIME] = 1
+
+        termios.tcsetattr(self.tty, when, mode)
+
+    def exit_raw(self):
+        termios.tcsetattr(self.tty, termios.TCSAFLUSH, self.savedmode)
+
+    def send_sequence(self, sequence, response=False):
+        command = str.encode('\033[' + sequence)
+        self.tty.write(command)
+        if response:
+            return self.read_response()
+
+    def read_response(self):
+        """Read a CSI """
+        output = b''
+        # read one byte from the stream
+        c = self.tty.read(1)
+
+        while c:
+            output += c
+            if c == b'R':
+                break
+            c = self.tty.read(1)
+
+        if output:
+            return output.decode('ascii')
+
+
+class Screen():
+
+    def __init__(self):
+        self.term = Terminal()
+        self.term.set_raw()
+        self.buffer = ''
+
+    def get_window_size(self):
+        self.term.send_sequence('999C')
+        self.term.send_sequence('999B')
+        self.get_cursor_pos()
+
+    def get_cursor_pos(self):
+        output = self.term.send_sequence('6n', True)[2:-1].split(';')
+        self.max_row, self.max_col = map(int, output)
+
+    def move_cursor(self, row, col):
+        # <ESC>[{ROW};{COLUMN}H
+        self.term.send_sequence('{};{}H'.format(row, col))
+
+    def write_status_msg(self, msg):
+        self.move_cursor(self.max_row-1, 0)
+        self.term.tty.write(str.encode(">>>--- STATUS BAR msg: {}".format(msg)))
+
+    def read_input(self):
+        pass
+
+    def refresh(self):
+        self.write_status_msg('AAAA')
+
+    def write_buffer(self):
+        self.move_cursor(1, 1)
+        self.term.tty.write(str.encode(self.buffer))
+
+class Editor():
+
+    def __init__(self):
+        self.screen = Screen()
+        self.screen.get_window_size()
+
+    def main_loop(self):
+        # refresh screen, write buffer
+        while True:
+
+            self.screen.write_buffer()
+            self.screen.refresh()
+
+            ch = self.screen.term.tty.read(1)
+            i = 0
+            self.screen.move_cursor(i, i)
+            i += 1
+            self.screen.term.tty.write(ch)
+
+
 
 if __name__ == '__main__':
-    with opentty() as tty:
-        with rawmode(tty, min=0, time=30):
-            tty.write(b'\033[6n')
-            print(_readyx(tty))
-
+    editor = Editor()
+    editor.main_loop()
